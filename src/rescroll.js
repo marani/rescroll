2efbf357cf29f40940e0a537468e028e31a94562
marani <dung1@e.ntu.edu.sg>
2015-06-20 11:13:05 +0700

    Update build script. Fix js syntax

--- src/rescroll.js
+++ src/rescroll.js
@@ -5,21 +5,21 @@ var Utils = {
     var viewPortWidth;
     var viewPortHeight;
     // the more standards compliant browsers (mozilla/netscape/opera/IE7) use window.innerWidth and window.innerHeight
-    if (typeof window.innerWidth != 'undefined') {
+    if (typeof window.innerWidth !== 'undefined') {
       viewPortWidth = window.innerWidth,
       viewPortHeight = window.innerHeight
     }
     // IE6 in standards compliant mode (i.e. with a valid doctype as the first line in the document)
-    else if (typeof document.documentElement != 'undefined' &&
-      typeof document.documentElement.clientWidth != 'undefined' &&
-      document.documentElement.clientWidth != 0) {
-      viewPortWidth = document.documentElement.clientWidth,
-      viewPortHeight = document.documentElement.clientHeight
+    else if (typeof document.documentElement !== 'undefined' &&
+      typeof document.documentElement.clientWidth !== 'undefined' &&
+      document.documentElement.clientWidth !== 0) {
+      viewPortWidth = document.documentElement.clientWidth;
+      viewPortHeight = document.documentElement.clientHeight;
     }
     // older versions of IE
     else {
-      viewPortWidth = document.getElementsByTagName('body')[0].clientWidth,
-      viewPortHeight = document.getElementsByTagName('body')[0].clientHeight
+      viewPortWidth = document.getElementsByTagName('body')[0].clientWidth;
+      viewPortHeight = document.getElementsByTagName('body')[0].clientHeight;
     }
     return {w: viewPortWidth, h: viewPortHeight};
   }
@@ -84,17 +84,17 @@ var Processor = {
             best = {
               anchor: anchors[i],
               value: rect.y1 - center.y
-            }
+            };
           else if (center.y > rect.y2 && center.y - rect.y2 < best.value)
             best = {
               anchor: anchors[i],
               value: center.y - rect.y2
-            }
+            };
         }
       }
       return this[best.anchor.type].focus(best.anchor.element, center);
     } else {
-      throw "Reflow Error - Anchors invalid";
+      throw 'Reflow Error - Anchors invalid';
     }
   },
   getNewSize: function(focus) {
@@ -139,12 +139,13 @@ var Processor = {
           pos: null,
           d: window.innerHeight
         };
+        var y1, y2, node, boundingRect;
         for (var i = 0; i < textNodes.length; i++) {
-          var node = textNodes[i];
-          var boundingRect = this.getBoundingRect(node);
+          node = textNodes[i];
+          boundingRect = this.getBoundingRect(node);
           if (!boundingRect) continue;
-          var y1 = boundingRect.y1;
-          var y2 = boundingRect.y2;
+          y1 = boundingRect.y1;
+          y2 = boundingRect.y2;
           if ((y1 <= center.y) && (y2 >= center.y)) {
             best.node = node;
             best.boundingRect = boundingRect;
